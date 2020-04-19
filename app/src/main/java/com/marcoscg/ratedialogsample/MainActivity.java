bdc043b22738e1c688fac266cf304f7da28a26bc
Marcos Calvo García <marcoscgdev@gmail.com>
2020-04-19 15:47:12 +0200

    Issue #3 was fixed

--- app/src/main/java/com/marcoscg/ratedialogsample/MainActivity.java
+++ app/src/main/java/com/marcoscg/ratedialogsample/MainActivity.java
@@ -12,15 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RateDialog.init(this); // Shows the dialog each 3 days and each 7 launches (default config)
-        //RateDialog.init(this, 2, RateDialog.DEFAULT_LAUNCHES_UNTIL_PROMPT);  // Shows the dialog with custom config.
+//        RateDialog.init(this, 2, RateDialog.DEFAULT_LAUNCHES_UNTIL_PROMPT);  // Shows the dialog with custom config.
 
         // Custom dialog instance
-//        RateDialog rateDialog = new RateDialog(this, "custom_rate_dialog");
+//        RateDialog rateDialog = new RateDialog(this, "custom_rate_dialog", 0 , 3);
 //        rateDialog.init();
     }
 
     public void openRateDialog (View v) {
-        RateDialog.showDialog(this); // Shows the dialog instantly
+        RateDialog rateDialog = new RateDialog(this, "custom_rate_dialog_key");
+        rateDialog.showDialog();
     }
 
 }
--- ratedialog/src/main/java/com/marcoscg/ratedialog/RateDialog.java
+++ ratedialog/src/main/java/com/marcoscg/ratedialog/RateDialog.java
@@ -5,11 +5,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AlertDialog;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -20,12 +20,12 @@ public class RateDialog {
 
     public static int DEFAULT_DAYS_UNTIL_PROMPT = 3;
     public static int DEFAULT_LAUNCHES_UNTIL_PROMPT = 7;
-    private static String DEFAULT_DIALOG_KEY = "ratedialog";
+    private static String DEFAULT_DIALOG_KEY = "default_rate_dialog_key";
 
     private Activity activity;
+    private AlertDialog alertDialog;
     private SharedPreferences prefs;
 
-    private String dialogKey;
     private int daysUntilPrompt;
     private int launchesUntilPrompt;
 
@@ -39,11 +39,12 @@ public class RateDialog {
 
     public RateDialog(Activity activity, String dialogKey, int daysUntilPrompt, int launchesUntilPrompt) {
         this.activity = activity;
-        this.dialogKey = dialogKey;
         this.daysUntilPrompt = daysUntilPrompt;
         this.launchesUntilPrompt = launchesUntilPrompt;
 
-        prefs = activity.getSharedPreferences(dialogKey, Context.MODE_PRIVATE);
+        prefs = activity.getSharedPreferences(dialogKey + "_rdp", Context.MODE_PRIVATE);
+
+        setupDialog();
     }
 
     public RateDialog(Activity activity, int daysUntilPrompt, int launchesUntilPrompt) {
@@ -96,26 +97,43 @@ public class RateDialog {
      * Public methods
      */
     public void init() {
-        if (prefs == null || prefs.getBoolean(dialogKey + "dontshowagain", false)) { return; }
-
-        long launch_count = prefs.getLong(dialogKey + "launch_count", 0) + 1;
-        prefs.edit().putLong(dialogKey + "launch_count", launch_count).apply();
-
-        long date_firstLaunch = prefs.getLong(dialogKey + "date_firstlaunch", 0);
-        if (date_firstLaunch == 0) {
-            date_firstLaunch = System.currentTimeMillis();
-            prefs.edit().putLong(dialogKey + "date_firstlaunch", date_firstLaunch).apply();
+        if (prefs == null || prefs.getBoolean("do_not_show_again", false)) {
+            return;
         }
 
-        if (launch_count >= launchesUntilPrompt) {
-            if (System.currentTimeMillis() >= date_firstLaunch +
+        long launchCount = prefs.getLong("launch_count", 0) + 1;
+        prefs.edit().putLong("launch_count", launchCount).apply();
+
+        long dateFirstLaunch = prefs.getLong("date_first_launch", 0);
+        if (dateFirstLaunch == 0) {
+            dateFirstLaunch = System.currentTimeMillis();
+            prefs.edit().putLong("date_first_launch", dateFirstLaunch).apply();
+        }
+
+        if (launchCount >= launchesUntilPrompt) {
+            if (System.currentTimeMillis() >= dateFirstLaunch +
                     (daysUntilPrompt * 24 * 60 * 60 * 1000)) {
-                show(activity);
+                showDialog();
             }
         }
     }
 
     public void showDialog() {
+        if (alertDialog != null) {
+            alertDialog.show();
+        }
+    }
+
+    /**
+     * Private methods
+     */
+    private void neverShow() {
+        if (prefs != null) {
+            prefs.edit().putBoolean("do_not_show_again", true).apply();
+        }
+    }
+
+    private void setupDialog() {
         LayoutInflater inflater = activity.getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_layout, null);
 
@@ -125,7 +143,7 @@ public class RateDialog {
         title.setText(String.format(activity.getResources().getString(R.string.rate_dialog_title), activity.getResources().getString(R.string.app_name)));
         message.setText(activity.getString(R.string.rate_dialog_message, activity.getResources().getString(R.string.app_name)));
 
-        AlertDialog dialog = new AlertDialog.Builder(activity)
+        alertDialog = new AlertDialog.Builder(activity)
                 .setView(view)
                 .setPositiveButton(activity.getResources().getString(R.string.rate_dialog_action_rate), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -134,6 +152,7 @@ public class RateDialog {
                         } catch (android.content.ActivityNotFoundException anfe) {
                             activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + activity.getPackageName())));
                         }
+
                         neverShow();
                         Toast.makeText(activity, activity.getResources().getString(R.string.rate_dialog_thank_you), Toast.LENGTH_SHORT).show();
                     }
@@ -144,39 +163,30 @@ public class RateDialog {
                         neverShow();
                     }
                 })
-                .show();
+                .create();
 
-        Typeface tf = Typeface.createFromAsset(activity.getAssets(),
-                "medium.ttf");
-
-        Button positivebt = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
-        Button neutralbt = dialog.getButton(AlertDialog.BUTTON_NEUTRAL);
-        Button negativebt = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
-
-        positivebt.setPadding(dpToPx(12, activity),0,dpToPx(12, activity),0);
-        positivebt.setTypeface(tf);
-
-        neutralbt.setPadding(dpToPx(12, activity),0,dpToPx(12, activity),0);
-        neutralbt.setTypeface(tf);
-
-        negativebt.setPadding(dpToPx(12, activity),0,dpToPx(12, activity),0);
-        negativebt.setTypeface(tf);
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                setButtonStyle(alertDialog.getButton(AlertDialog.BUTTON_POSITIVE));
+                setButtonStyle(alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL));
+                setButtonStyle(alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE));
+            }
+        });
     }
 
-    /**
-     * Private methods
-     */
-    private void neverShow() {
-        if (prefs != null) {
-            prefs.edit().putBoolean(dialogKey + "dontshowagain", true).apply();
+    private void setButtonStyle(Button button) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            button.setPadding(dpToPx(14),0, dpToPx(14),0);
+        } else {
+            button.setPadding(dpToPx(12),0, dpToPx(12),0);
         }
+
+        button.setTypeface(Typeface.createFromAsset(activity.getAssets(), "medium.ttf"));
     }
 
-    private static int dpToPx(int dp, Activity activity) {
-        if (Build.VERSION.SDK_INT >= 21)
-            dp = dp+2;
-        DisplayMetrics displayMetrics = activity.getResources().getDisplayMetrics();
-        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    private int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
     }
 
 }
